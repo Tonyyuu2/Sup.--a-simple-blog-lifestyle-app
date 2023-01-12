d4751afc141ad4aa388dec73946e2862c20a2ca9
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-12 13:34:55 -0800

    add hover on plus icon

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -10,10 +10,14 @@ function Navbar() {
       <nav className="w-screen h-20 bg-red-500 text-white ">
         {/* mobile view */}
         <div className="md:hidden">
-          <div className="flex justify-between p-3.5 items-center ">
-            <img src={hamburgerMenuIcon} alt="" className="w-10" />
+          <div className="flex justify-between p-3 items-center ">
+            <img src={hamburgerMenuIcon} alt="" className="w-10 pt-2.5" />
             <h1 className="font-bold text-4xl items pl-5">Sup.</h1>
-            <img src={plusIcon} alt="" className="w-10" />
+            <img
+              src={plusIcon}
+              alt=""
+              className="w-10 transition ease-in-out delay-10 pt-2.5 hover:-translate-y-1 hover:scale-110 duration-300"
+            />
           </div>
         </div>
 
@@ -21,9 +25,9 @@ function Navbar() {
         <div className="hidden md:inline">
           <div className="flex p-3 pl-12 justify-between">
             <h1 className="flex font-bold text-5xl ">Sup.</h1>
-            <div className="flex justify-between w-1/3 p-6 text-xl md:text-[0.9em] md:p-6 md:font-bold md:w-[20em]">
+            <div className="flex justify-between w-1/3 p-6 lg:text-xl md:text-md md:p-6 md:font-bold md:w-[20em]">
               <Link to="">
-                <p>Show All Posts</p>
+                <p>Show All Posts!</p>
               </Link>
               <Link to="">
                 <p>Add A Post!</p>
