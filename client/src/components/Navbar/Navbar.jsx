bdeb37cec6c1cd72072f06e63764a0ca89e03be4
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-12 12:41:24 -0800

    start navbar

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="w-screen h-20 bg-red-500 text-white">
+      <div className="flex">
+        <div className="flex font-bold text-5xl p-3 pl-5">
+          <h1>Sup.</h1>
+        </div>
+        <div className="flex">
+          
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
