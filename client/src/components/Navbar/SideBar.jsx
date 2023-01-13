46ecf2f24c87c68d32221ce77d303ed012dd121c
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-13 15:57:59 -0800

    finish sidebar for now

--- client/src/components/Navbar/SideBar.jsx
+++ client/src/components/Navbar/SideBar.jsx
@@ -16,7 +16,8 @@ function SideBar({ openSideBar, setOpenSideBar }) {
           animate={openSideBar ? "open" : "closed"}
           variants={variants}
           transition={{ duration: 0.5 }}
-          className={`top-0 left-0 w-[45%] bg-[#F2F2F2] text-black fixed h-full -translate-x-full transition-all `}
+          className={`top-0 left-0 w-[45%] bg-[#F2F2F2] text-black fixed h-full -translate-x-full transition-all ease-out`}
+          // id='translate-x-0 transition-all'
         >
           <div className="flex flex-col p-6">
             <div className="flex justify-end">
