4da3f7b7ebc10b0a023dd7277e78415d89108a4d
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-13 21:32:09 -0800

    add spacing

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -14,7 +14,7 @@ function Posts() {
   })
 
   return (
-    <div className='absolute flex flex-col justify-center items center p-8 gap-20 lg:left-[22.5%] md:left-[12.5%]'>
+    <div className='absolute flex flex-col justify-center items center p-8 gap-20 lg:left-[22.5%] md:left-[12.5%] mt-5'>
       {postListItems}
     </div>
   )
