9893f6547a1b5b8a52524d3cfe14a9abe4819e18
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-15 11:38:59 -0800

    update form

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import Posts from '../Posts/Posts'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Posts from "../Posts/Posts";
 
 function Dashboard() {
   return (
     <div>
+      <Outlet />
       <Posts />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- client/src/components/NewPost.jsx/NewPost.jsx
+++ client/src/components/NewPost.jsx/NewPost.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function NewPost() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState('')
+
   return (
-    <div>
+    <div className="flex justify-center items-center mt-6">
       {/* card container */}
-      
+      <div className="bg-red-300 w-8/12 md:w-[680px] h-auto p-6">
+        {/* form */}
+        <div className="flex flex-col">
+          <h1>Whatcha get up to?</h1>
+
+          <form className="flex flex-col">
+            <label>Title</label>
+            <input
+              required
+              type="text"
+              value={title}
+              onChange={(event) => setTitle(event.target.value)}
+              placeholder="Post Title"
+            />
+            <label>Description</label>
+            <textarea
+              required
+              type="text"
+              value={description}
+              onChange={(event) => setTitle(event.target.value)}
+              placeholder="What happened?"
+            />
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default NewPost
+export default NewPost;
