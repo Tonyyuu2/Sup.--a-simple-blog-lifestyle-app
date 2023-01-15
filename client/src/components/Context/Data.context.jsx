d920e4c0deed02c7788dcd7d1d85de3ec7d1fd2a
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-15 14:06:08 -0800

    create context for data

--- client/src/components/Context/Data.context.jsx
+++ client/src/components/Context/Data.context.jsx
@@ -1,4 +1,20 @@
 import { createContext, useState, useEffect } from "react";
 
+import { mockData } from "../../mockData";
 
+export const DataContext = createContext({
+  data: [],
+  setData: () => {},
+});
 
+export const DataProvider = ({ children }) => {
+  const [data, setData] = useState(mockData);
+  console.log("data :", data);
+
+  const value = {
+    data,
+    setData,
+  };
+
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
