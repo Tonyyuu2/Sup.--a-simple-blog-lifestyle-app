5525bc1a2184b680c345b7dfb319255883283571
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-16 14:14:33 -0800

    finish edit feature

--- client/src/components/Context/Data.context.jsx
+++ client/src/components/Context/Data.context.jsx
@@ -5,17 +5,25 @@ import { mockData } from "../../mockData";
 export const DataContext = createContext({
   data: [],
   addData: () => {},
+  editData: (id, updatedData) => {}
 });
 
 export const DataProvider = ({ children }) => {
   const [data, setData] = useState(mockData);
   console.log("data :", data);
 
+  const updateData = ( id , updatedData ) => {
 
+    const withoutCurrentPost = data.filter((post) => post.id !== id);
+
+    setData([updatedData, ...withoutCurrentPost])
+
+  }
 
   const value = {
     data,
     addData: newData => setData((prev) => [...prev, newData]),
+    editData: updateData
   };
 
   return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
--- client/src/components/EditPost/EditPost.jsx
+++ client/src/components/EditPost/EditPost.jsx
@@ -1,9 +1,18 @@
+import { toBeInTheDocument } from '@testing-library/jest-dom/dist/matchers';
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
+import Button from '../Button/Button';
 import { DataContext } from '../Context/Data.context'
 import ErrorHandling from '../ErrorHandling/ErrorHandling'
 
 function EditPost() {
+
+  const { editData } = useContext(DataContext)
+
+  const newDate = new Date();
+  const currentDate = newDate.toISOString().split("T")[0];
+
+
   const [ info, setInfo ] = useState({
     id: '',
     title: '',
@@ -11,8 +20,15 @@ function EditPost() {
     location: '',
     date: '',
     image: '',
-  })
-  console.log('info :', info);
+  });
+
+  const [error, setError] = useState({
+    titleError: false,
+    descriptionError: false,
+    locationError: false,
+    dateError: false,
+    imageError: false,
+  });
 
   const { data } = useContext(DataContext)
   const navigate = useNavigate();
@@ -30,13 +46,161 @@ function EditPost() {
       location: filteredData[0]?.location,
       date: filteredData[0]?.date,
       image: filteredData[0]?.image,
-
     })
   }, [])
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (info.title === "" || info.title.length <= 5) {
+      const titleErrorNew = (error.titleError = true);
+      setError((prev) => ({ ...prev, titleErrorNew }));
+      return;
+    } else {
+      const titleErrorNew = (error.titleError = false);
+      setError((prev) => ({ ...prev, titleErrorNew }));
+    }
+
+    if (info.description === "" || info.description.length <= 10) {
+      const newDescriptionError = (error.descriptionError = true);
+      setError((prev) => ({ ...prev, newDescriptionError }));
+      return;
+    } else {
+      const newDescriptionError = (error.descriptionError = false);
+      setError((prev) => ({ ...prev, newDescriptionError }));
+    }
+
+    if (info.location === "" || info.location.length <= 10) {
+      const newLocationError = (error.locationError = true);
+      setError((prev) => ({ ...prev, newLocationError }));
+      return;
+    } else {
+      const newLocationError = (error.locationError = false);
+      setError((prev) => ({ ...prev, newLocationError }));
+    }
+
+    if (info.date === "yyyy-mm-dd" || info.date > currentDate || info.date < currentDate) {
+      const newDateError = (error.dateError = true);
+      setError((prev) => ({ ...prev, newDateError }));
+      return;
+    } else if (info.date === currentDate) {
+      const newDateError = (error.dateError = false);
+      setError((prev) => ({ ...prev, newDateError }));
+    }
+
+    const regex =
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+    if (info.image === "" || info.image.length <= 50 || !info.image.match(regex)) {
+      const newImageError = (error.imageError = true);
+      setError((prev) => ({ ...prev, newImageError }));
+      return;
+    } else if (info.date === currentDate) {
+      const newImageError = (error.imageError = false);
+      setError((prev) => ({ ...prev, newImageError }));
+    }
+
+    const newData = {
+      id: info.id,
+      title: info.title,
+      description: info.description,
+      location: info.location,
+      date: info.date,
+      image: info.image
+    };
+
+    editData(id, newData)
+    navigate("/");
+  };
+
+  const handleCancel = () => {
+    navigate('/')
+  }
+
 
   return (
-    <div>{info.title}</div>
+    <div className="flex justify-center items-center mt-6 mb-8 p-6">
+      {/* card container */}
+      <div className="bg-red-300 w-8/12 md:w-[680px] h-auto p-6 rounded-xl">
+        {/* form */}
+        <div className="flex flex-col gap-5">
+          <i className="font-bold text-white text-4xl">Whatcha get up to?</i>
+
+          <form className="flex flex-col">
+            <label className="font-bold text-lg">Title</label>
+            <input
+              required
+              type="text"
+              value={info.title}
+              onChange={(event) => setInfo((prev) => {
+                return {...prev, title: event.target.value}
+              })}
+              placeholder="Post Title"
+              className="p-2 rounded-xl mb-3"
+            />
+            {error.titleError && <ErrorHandling title={true} />}
+            <label className="font-bold text-lg">Description</label>
+            <textarea
+              required
+              type="text"
+              value={info.description}
+              autoComplete="off"
+              rows="5"
+              onChange={(event) => setInfo((prev) => {
+                return {...prev, description: event.target.value}
+              })}
+              placeholder="What happened?"
+              className="p-2 rounded-xl mb-3"
+            />
+            {error.descriptionError && <ErrorHandling description={true} />}
+            <label className="font-bold text-lg">Location</label>
+            <input
+              required
+              type="text"
+              value={info.location}
+              onChange={(event) => setInfo((prev) => {
+                return {...prev, location: event.target.value}
+              })}
+              placeholder="Where at?"
+              className="p-2 rounded-xl mb-3"
+            />
+            {error.locationError && <ErrorHandling location={true} />}
+            <label className="font-bold text-lg">Date</label>
+            <input
+              required
+              type="date"
+              value={info.date}
+              onChange={(event) => setInfo((prev) => {
+                return {...prev, date: event.target.value}
+              })}
+              className="p-2 rounded-xl mb-3"
+            />
+            {error.dateError && <ErrorHandling date={true} />}
+            <label className="font-bold text-lg">Image</label>
+            <input
+              required
+              type="url"
+              value={info.image}
+              onChange={(event) => setInfo((prev) => {
+                return {...prev, image: event.target.value}
+              })}
+              placeholder="Add Link To Image"
+              pattern="https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+              onInvalid={(e) =>
+                e.target.setCustomValidity("Please Enter a Valid URL image.")
+              }
+              onInput={(e) => e.target.setCustomValidity("")}
+              className="p-2 rounded-xl mb-3"
+            />
+            {error.imageError && <ErrorHandling image={true} />}
+            <div className="flex justify-center items-center p-3 mt-5 gap-4">
+              <Button children="Post" onSubmit={handleSubmit} />
+              <Button children="Cancel" cancel={true} onCancel={handleCancel}/>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
