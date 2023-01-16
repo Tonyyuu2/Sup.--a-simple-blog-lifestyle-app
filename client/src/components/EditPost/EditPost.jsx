de30c0564adbdd6d8233ed39319e93fccbfb4359
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-16 12:51:20 -0800

    start accepting data from param query + store in state

--- client/src/components/EditPost/EditPost.jsx
+++ client/src/components/EditPost/EditPost.jsx
@@ -4,11 +4,34 @@ import { DataContext } from '../Context/Data.context'
 import ErrorHandling from '../ErrorHandling/ErrorHandling'
 
 function EditPost() {
+  const [ info, setInfo ] = useState({
+    id: '',
+    title: '',
+    description: '',
+    location: '',
+    date: '',
+    image: '',
+  })
+  console.log('info :', info);
 
   const { data } = useContext(DataContext)
   const navigate = useNavigate();
 
   const { id } = useParams();
+
+  useEffect(() => {
+
+    const filteredData = data.filter((post) => post.id === id)
+    console.log('filteredData :', filteredData);
+
+    setInfo({
+      id: filteredData[0].id,
+      title: filteredData[0].title,
+      description: filteredData[0].description,
+      location: filteredData[0].location,
+
+    })
+  }, [])
 
 
   return (
