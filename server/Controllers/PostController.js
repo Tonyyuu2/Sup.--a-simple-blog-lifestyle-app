d545179dd9995195acd087b5b103b96f6533e7eb
Tony Yu <tony.kwangjong.yu@gmail.com>
2023-01-17 13:55:51 -0800

    add cors

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,9 +1,9 @@
 import PostModel from "../Models/postModel.js";
 
 export const getAllPosts = async (req, res) => {
+  const posts = await PostModel.find({});
   
   try {
-    const posts = await PostModel.find({});
     res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import router from "./Routes/PostsRoute.js";
+import cors from "cors";
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
 const app = express();
 mongoose.set('strictQuery', false)
 
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb", extended: true }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 dotenv.config();
